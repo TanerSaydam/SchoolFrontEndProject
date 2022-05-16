12dd913506b2be03791ffaf558d73aa50521ffc2
Taner Saydam <tanersaydam@gmail.com>
2022-05-16 13:54:00 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminClassesComponent } from './Components/admin/admin-classes/admin-classes.component';
-import { AdminLayoutComponent } from './Components/admin/admin-layout/admin-layout.component';
-import { AdminLessonsComponent } from './Components/admin/admin-lessons/admin-lessons.component';
-import { AdminLoginComponent } from './Components/admin/admin-login/admin-login.component';
-import { AdminStudensComponent } from './Components/admin/admin-studens/admin-studens.component';
-import { AdminTeachersComponent } from './Components/admin/admin-teachers/admin-teachers.component';
-import { AdminComponent } from './Components/admin/admin.component';
-import { HomeComponent } from './Components/home/home.component';
-import { StudentsLayoutComponent } from './Components/students/students-layout/students-layout.component';
-import { StudentsLecturenotesComponent } from './Components/students/students-lecturenotes/students-lecturenotes.component';
-import { StudentsLoginComponent } from './Components/students/students-login/students-login.component';
-import { StudentsReportcardComponent } from './Components/students/students-reportcard/students-reportcard.component';
-import { StudentsTestsComponent } from './Components/students/students-tests/students-tests.component';
-import { StudentsComponent } from './Components/students/students.component';
-import { AnnouncementsComponent } from './Components/teachers/announcements/announcements.component';
-import { LecturenotesComponent } from './Components/teachers/lecturenotes/lecturenotes.component';
-import { TeachersLayoutComponent } from './Components/teachers/teachers-layout/teachers-layout.component';
-import { TeachersLoginComponent } from './Components/teachers/teachers-login/teachers-login.component';
-import { TeachersComponent } from './Components/teachers/teachers.component';
-import { TestsComponent } from './Components/teachers/tests/tests.component';
+import { AdminClassesComponent } from './components/admin/admin-classes/admin-classes.component';
+import { AdminLayoutComponent } from './components/admin/admin-layout/admin-layout.component';
+import { AdminLessonsComponent } from './components/admin/admin-lessons/admin-lessons.component';
+import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
+import { AdminStudensComponent } from './components/admin/admin-studens/admin-studens.component';
+import { AdminTeachersComponent } from './components/admin/admin-teachers/admin-teachers.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { StudentsLayoutComponent } from './components/students/students-layout/students-layout.component';
+import { StudentsLecturenotesComponent } from './components/students/students-lecturenotes/students-lecturenotes.component';
+import { StudentsLoginComponent } from './components/students/students-login/students-login.component';
+import { StudentsReportcardComponent } from './components/students/students-reportcard/students-reportcard.component';
+import { StudentsTestsComponent } from './components/students/students-tests/students-tests.component';
+import { StudentsComponent } from './components/students/students.component';
+import { AnnouncementsComponent } from './components/teachers/announcements/announcements.component';
+import { LecturenotesComponent } from './components/teachers/lecturenotes/lecturenotes.component';
+import { TeachersLayoutComponent } from './components/teachers/teachers-layout/teachers-layout.component';
+import { TeachersLoginComponent } from './components/teachers/teachers-login/teachers-login.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { TestsComponent } from './components/teachers/tests/tests.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
